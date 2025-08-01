546f3101d102d8cc98c8f5ec6aa63d4ede2314cd
fsncps <fsncps@eml.cc>
2025-08-01 08:55:52 +0200

    search and add

--- cmd/zeno/main.go
+++ cmd/zeno/main.go
@@ -8,6 +8,15 @@ import (
 )
 
 func main() {
+    args := os.Args[1:]
+    if len(args) > 0 && args[0] == "search" {
+        if err := tui.RunSearch(); err != nil {
+            fmt.Fprintln(os.Stderr, "Error:", err)
+            os.Exit(1)
+        }
+        return
+    }
+    // default to add
     if err := tui.RunAdd(); err != nil {
         fmt.Fprintln(os.Stderr, "Error:", err)
         os.Exit(1)
